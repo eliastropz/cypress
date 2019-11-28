b0b8d7a3ff83d0ccc64f0b495f6e792603e0d8b9
Elias Siqueira <eliaslsiqueira@gmail.com>
2019-11-28 10:18:31 -0400

    First test with cypress

---
+++ curso-cypress/cypress/integration/primeiro_teste.spec.js
@@ -0,0 +1,8 @@
+///  <reference types="cypress" /> 
+
+
+describe('Meu primeiro teste', function() {
+    it('Acessar a página de exemplos do cypress', function() {
+      cy.visit('https://example.cypress.io')
+    })
+  })
