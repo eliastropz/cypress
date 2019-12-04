2a7708c2937bcd75bf8745e918cc9125fb1e80b9
Elias Siqueira <eliaslsiqueira@gmail.com>
2019-12-04 05:47:44 -0400

    locators

--- curso-cypress/cypress/integration/barriga/functional.spec.js
+++ curso-cypress/cypress/integration/barriga/functional.spec.js
@@ -1,17 +1,23 @@
 ///  <reference types="cypress" /> 
 
+import loc from '../../support/locators'
 
 describe('Should test at a functional level', () => {
     before(() => {
         cy.visit('https://barrigareact.wcaquino.me/')
-        cy.get('[data-test=email]').type('tropz')
-        cy.get('[data-test=passwd]').type('123456789')
-        cy.get('.btn').click()
-        cy.get('.toast-message').should('contain', 'Bem vindo')
+        cy.get(loc.LOGIN.USER).type('tropz')
+        cy.get(loc.LOGIN.PASSWORD).type('123456789')
+        cy.get(loc.LOGIN.BTN_LOGIN).click()
+        cy.get(loc.MESSAGE).should('contain', 'Bem vindo')
 
     })
 
     it('Should create an account', () => {
+        cy.get('[data-test=menu-settings]').click()
+        cy.get('[href="/contas"]').click()
+        cy.get('[data-test=nome]').type('Conta de teste')
+        cy.get('.btn').click()
+        cy.get('.toast-message').should('contain', 'Conta inserida com sucesso')
 
     })
 })
